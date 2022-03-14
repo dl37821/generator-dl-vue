9a8736d47bea846a8875503ed68cf6955ff31531
dinglei <dinglei@wsecar.com>
2022-03-14 17:49:27 +0800

    add

--- generators/app/index.js
+++ generators/app/index.js
@@ -5,6 +5,12 @@ module.exports = class extends Generator{
     return this.prompt([
       {
         type: 'input',
+        name: 'version',
+        message: 'Choose a version of Vue.js that you want to start the project with (Use arrow keys)',
+        default: '3.x'
+      },
+      {
+        type: 'input',
         name: 'name',
         message: 'Your project name',
         default: this.appname
@@ -16,22 +22,38 @@ module.exports = class extends Generator{
   }
 
   writing () {
-    const templates = [
-      'babel.config.js',
-      'package.json',
-      'README.md',
-      'public/favicon.ico',
-      'public/index.html',
-      'src/App.vue',
-      'src/main.js',
-      'src/assets/logo.png',
-      'src/components/HelloWorld.vue',
-    ]
-
+    console.log('答案==', this.answers)
+    const { version } = this.answers
+    let templates = []
+    if (version.includes('3')) {
+      templates = [
+        'vue3/babel.config.js',
+        'vue3/package.json',
+        'vue3/README.md',
+        'vue3/public/favicon.ico',
+        'vue3/public/index.html',
+        'vue3/src/App.vue',
+        'vue3/src/main.ts',
+        'vue3/src/assets/logo.png',
+        'vue3/src/components/HelloWorld.vue',
+      ]
+    } else {
+      templates = [
+        'vue2/babel.config.js',
+        'vue2/package.json',
+        'vue2/README.md',
+        'vue2/public/favicon.ico',
+        'vue2/public/index.html',
+        'vue2/src/App.vue',
+        'vue2/src/main.js',
+        'vue2/src/assets/logo.png',
+        'vue2/src/components/HelloWorld.vue',
+      ]
+    }
     templates.forEach(item => {
       this.fs.copyTpl(
         this.templatePath(item),
-        this.destinationPath(item),
+        this.destinationPath(item.split(version.includes('3') ? 'vue3/' : 'vue2/')[1]),
         this.answers
       ) 
     })
--- generators/app/templates/vue3/README.md
+++ generators/app/templates/vue3/README.md
@@ -1,4 +1,4 @@
-# <%= htmlWebpackPlugin.options.title %>
+# <%= name %>
 
 ## Project setup
 ```
@@ -15,3 +15,10 @@ npm run serve
 npm run build
 ```
 
+### Lints and fixes files
+```
+npm run lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
